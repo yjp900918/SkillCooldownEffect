a6cd283de916dea636e9ef47242cd35cf12a6f8d
yjp900918 <yjp900918@gmail.com>
2018-04-01 12:05:01 -0400

    5555

---
+++ Assets/SkillCoolDown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCoolDown : MonoBehaviour {
+
+    public float cooldownTime = 2;
+    float timer;
+    public Image image;
+    public KeyCode key;
+
+	// Use this for initialization
+	void Start () {
+        timer = cooldownTime;
+	}
+	
+    public void CastSkill()
+    {
+        if(timer > cooldownTime)
+        {
+            Debug.Log("Cast skill!!!");
+            timer = 0;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        image.fillAmount = (cooldownTime - timer) / cooldownTime;
+        if (Input.GetKeyDown(key))
+        {
+            CastSkill();
+        }
+	}
+}
